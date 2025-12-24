7a11c7b007d566a5eb6bebe7a9d2afaae43df901
Ezzedin M.S. Hassoun <ezzedinhassoun_1994@hotmail.com>
2025-12-24 11:47:01 +0200

    header

---
+++ Client/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Pages/HomePage";
+import LoginRegPage from "./Pages/LoginRegPage";
+import LoginFormComponent from "./Components/LoginFormComponent";
+import RegistarionComponent from "./Components/RegistarionComponent";
+import Header from "./Components/Header";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<LoginRegPage />}>
+          <Route path="login" element={<LoginFormComponent />} />
+          <Route path="register" element={<RegistarionComponent />} />
+          <Route path="*" element={<Navigate to="/auth/login" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
