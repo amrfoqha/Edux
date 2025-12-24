2c9b490f54677f09a60e4849ffe9c54b5cd74488
Ezzedin M.S. Hassoun <ezzedinhassoun_1994@hotmail.com>
2025-12-24 15:42:27 +0200

    footer&header

--- Client/src/Pages/LoginRegPage.jsx
+++ Client/src/Pages/LoginRegPage.jsx
@@ -7,6 +7,7 @@ const LoginRegPage = () => {
 
   return (
     <>
+      <Header />
 
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-10 px-4">
         <div className="w-full max-w-md bg-white rounded-xl shadow-md p-8">
@@ -126,6 +127,7 @@ const LoginRegPage = () => {
         </div>
       </div>
 
+      <Footer />
     </>
   );
 };
