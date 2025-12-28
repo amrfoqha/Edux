29383eeb82f1f22aab58b1a4aaa5f468ad4dc4c4
Amr <amr20012200131@hotmail.com>
2025-12-28 11:14:37 +0200

    added the the chat on the client side

--- Client/src/API/ResouceAPI.jsx
+++ Client/src/API/ResouceAPI.jsx
@@ -1,13 +1,5 @@
 import api from "./baseUrl.jsx";
 
-import axios from "axios";
-
-const BASE_URL = "http://localhost:8000/api/resources";
-
-export const getResourceById = async (id) => {
-  const res = await axios.get(`${BASE_URL}/${id}`);
-  return res.data;
-};
 
 export const uploadResource = async (data) => {
   try {
